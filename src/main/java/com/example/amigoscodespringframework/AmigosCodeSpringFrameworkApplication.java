25c262f6579ecd54116a215af80af8c1ec8224eb
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-09-22 17:04:43 -0300

    feat: hello world controller :)

--- src/main/java/com/example/amigoscodespringframework/AmigosCodeSpringFrameworkApplication.java
+++ src/main/java/com/example/amigoscodespringframework/AmigosCodeSpringFrameworkApplication.java
@@ -2,12 +2,22 @@ package com.example.amigoscodespringframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AmigosCodeSpringFrameworkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmigosCodeSpringFrameworkApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public List<String> hello() {
+        return List.of("Hello", "World!");
     }
 
 }
