5e9eb4e0198c4ee087dc8df074c144fc1fec46ab
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-09-22 16:52:20 -0300

    init project

---
+++ src/main/java/com/example/amigoscodespringframework/AmigosCodeSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodespringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosCodeSpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigosCodeSpringFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amigoscodespringframework/AmigosCodeSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodespringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosCodeSpringFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
