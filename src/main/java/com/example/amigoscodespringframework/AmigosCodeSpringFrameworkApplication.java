da865a246ce0e9f6228be0d29b2d04e75b6911c5
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-09-22 17:16:31 -0300

    feat: Api Layer

--- src/main/java/com/example/amigoscodespringframework/AmigosCodeSpringFrameworkApplication.java
+++ src/main/java/com/example/amigoscodespringframework/AmigosCodeSpringFrameworkApplication.java
@@ -11,24 +11,10 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class AmigosCodeSpringFrameworkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmigosCodeSpringFrameworkApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public List<Student> hello() {
-        return List.of(
-                new Student(1L,
-                        "Emanuel",
-                        "emanuel@gmail.com",
-                        20, LocalDate.of(2002,
-                        Month.MAY,
-                        19)
-                )
-        );
     }
 
 }
