430dd0e67157d0fea729bbcb13cec35fa16820de
Emanuelvss13 <emanuelsouza2002@gmail.com>
2022-09-22 22:03:54 -0300

    feat: Createing Students

---
+++ src/main/java/com/example/amigoscodespringframework/student/StudentConfig.java
@@ -0,0 +1,28 @@
+package com.example.amigoscodespringframework.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+    return args -> {
+
+        Student emanuel = new Student(1L, "Emanuel", "emanuel@email.com", 20, LocalDate.of(2002, Month.MAY, 19));
+        Student joao = new Student(2L, "João", "joão@email.com", 20, LocalDate.of(2002, Month.JANUARY, 19));
+
+        studentRepository.saveAll(
+                List.of(emanuel, joao)
+        );
+
+        };
+    }
+
+}
